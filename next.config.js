66dfc51aa00525c8969addb02cea19beeddf206e
spacewaterbear <emmanuel.thiery.pro@gmail.com>
2026-04-07 12:11:52 +0200

    main:style: remove useless stuff

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        instrumentationHook: true,
-    },
     allowedDevOrigins: ["*.preview.same-app.com"],
     output: "standalone",
     images: {
